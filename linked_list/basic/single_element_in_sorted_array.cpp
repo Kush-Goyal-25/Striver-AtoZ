c22d57a5cff8319da454a84e9e1baca71f083047
Kush-Goyal-25 <kush.goyal.personal@gmail.com>
2025-05-26 17:42:38 +0530

    adding new code

---
+++ linked_list/basic/single_element_in_sorted_array.cpp
@@ -0,0 +1,31 @@
+#include <algorithm>
+#include <bits/stdc++.h>
+#include <climits>
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#define ll long long
+using namespace std;
+
+
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int left = 0;
+    int right = nums.size() - 1;
+
+    while (left < right) {
+        int mid = left + (right - left) / 2;
+
+        if (mid % 2 == 1)
+            mid--;
+
+        if (nums[mid] == nums[mid + 1])
+            left = mid + 2;
+        else
+            right = mid;
+    }
+
+    return nums[left];
+    }
+};
---
+++ linked_list/strings/remove_outermost_brackets.cpp
@@ -0,0 +1,33 @@
+#include <algorithm>
+#include <bits/stdc++.h>
+#include <climits>
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#define ll long long
+using namespace std;
+
+
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string result;
+        int depth = 0;
+
+        for (char ch : s) {
+            if (ch == '(') {
+                depth++;
+                if (depth > 1) {
+                    result += ch;
+                }
+            } else if (ch == ')') {
+                depth--;
+                if (depth >= 1) {
+                    result += ch;
+                }
+            }
+        }
+
+        return result;
+    }
+};
